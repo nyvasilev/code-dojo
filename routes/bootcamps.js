7ed51824a0833aa7f3c6a73e55d07782cf6ca99c
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-13 18:00:07 +0300

    express router setup

---
+++ routes/bootcamps.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/v1/bootcamps", (_req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+router.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+router.post("/api/v1/bootcamps", (_req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+router.put("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+router.delete("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
