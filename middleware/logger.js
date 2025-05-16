0fc27b2b381300dab6a29357fd86156eb4a6750b
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-16 22:40:02 +0300

    add middleware

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+// @desc   Logs reqs to console
+export const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`,
+  );
+
+  next();
+};
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import bootcamps from "./routes/bootcamps.js";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Routes
 app.use("/api/v1/bootcamps", bootcamps);
