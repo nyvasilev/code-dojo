7e6e7910bd03ee906bb225eeb058a94c7c51293b
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-24 23:46:19 +0300

    custom error handler

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,18 @@
 export const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
   // Log to console for dev
   console.log(err.stack);
 
-  res.status(500).json({
+  // Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  res.status(err.statusCode || 500).json({
     success: false,
-    error: err.message,
+    error: err.message || "Server error",
   });
 };
