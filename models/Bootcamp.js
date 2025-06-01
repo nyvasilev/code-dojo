4e9b9add669f0b9e6b07969b27ea46d86318b84a
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-06-01 23:07:14 +0300

    disable geolocation at the moment

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import slugify from "slugify";
-import { geocoder } from "../utils/geocoder";
+import { geocoder } from "../utils/geocoder.js";
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -112,22 +112,22 @@ BootcampSchema.pre("save", async function (next) {
 });
 
 // Geocode & create location field
-BootcampSchema.pre("save", async function (next) {
-  const loc = await geocoder.geocode(this.address);
-  this.location = {
-    type: "Point",
-    coordinates: [loc[0].longitude, loc[0].latitude],
-    formattedAddress: loc[0].formattedAddress,
-    street: loc[0].streetName,
-    city: loc[0].city,
-    state: loc[0].stateCode,
-    zipcode: loc[0].zipcode,
-    country: loc[0].countryCode,
-  };
-
-  // Do not save address in DB
-  this.address = undefined;
-  next();
-});
+// BootcampSchema.pre("save", async function (next) {
+//   const loc = await geocoder.geocode(this.address);
+//   this.location = {
+//     type: "Point",
+//     coordinates: [loc[0].longitude, loc[0].latitude],
+//     formattedAddress: loc[0].formattedAddress,
+//     street: loc[0].streetName,
+//     city: loc[0].city,
+//     state: loc[0].stateCode,
+//     zipcode: loc[0].zipcode,
+//     country: loc[0].countryCode,
+//   };
+//
+//   // Do not save address in DB
+//   this.address = undefined;
+//   next();
+// });
 
 export const Bootcamp = mongoose.model("Bootcamp", BootcampSchema);
