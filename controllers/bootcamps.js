e2d430b275b1fdbd543f80fb197c3d0dcd470eb4
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-06-02 23:09:48 +0300

    filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,19 @@ import { asyncHandler } from "../middleware/async.js";
 // @route        GET /api/v1/bootcamps
 // @access       Public
 export const getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  // Looking for: >,>=, <, <=, search a list
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`,
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
 
   res
     .status(200)
