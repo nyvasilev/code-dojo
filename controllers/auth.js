f564b04d884115026aa5e596a24b1065e4ef3399
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-06-26 23:23:05 +0300

    sign and get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,15 +5,18 @@ import { asyncHandler } from "../middleware/async.js";
 // @desc         Register user
 // @route        GET /api/v1/auth/register
 // @access       Public
-export const register = asyncHandler(async (req, res, next) =>{
+export const register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
 
-    const { name, email, password, role } = req.body
+  // Create user
+  const user = User.create({
+    name,
+    email,
+    password,
+    role,
+  });
 
+  const token = user.getSignedJwtToken();
 
-    // Create user
-    const user = User.create({
-        name, email, password, role
-    })
-
-    res.status(200).json({ success: true})
-}
+  res.status(200).json({ success: true, token });
+});
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -50,6 +51,13 @@ UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
+
+  // Sign JWT and return
+  UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+  };
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
